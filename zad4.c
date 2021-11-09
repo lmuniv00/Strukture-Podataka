c7c4fb4d8694a386e197320e52314ca189ed25e2
lmuniv00 <92435471+lmuniv00@users.noreply.github.com>
2021-11-09 11:28:23 +0100

    Update zad4.c

--- zad4.c
+++ zad4.c
@@ -26,87 +26,86 @@ int CreateAndInsertAfter(int coefficient, int exponent, Position position);
 int ResultMultiply(Position Multiplication, Position head1, Position head2);
 
 int main() {
-	
 	Element head1 = { .coefficient = 0, .exponent = 0, .next = NULL };
 	Element head2 = { .coefficient = 0, .exponent = 0, .next = NULL };
 	Element Addition = { .coefficient = 0, .exponent = 0, .next = NULL };
 	Element Multiplication = { .coefficient = 0, .exponent = 0, .next = NULL };
 	char fileName[MAX_SIZE];
-    printf("Enter file name: ");
+    	printf("Enter file name: ");
 	scanf(" %s", fileName);
 	ReadFile(&head1, &head2, fileName);
-  PrintPoly("First polinome: ", head1.next);
+  	PrintPoly("First polinome: ", head1.next);
 	PrintPoly("Second polinome: ", head2.next);
 	ResultAdd(&Addition, &head1, &head2);
 	ResultMultiply(&Multiplication, &head1, &head2);
-  PrintPoly("Result of addition is: ", Addition.next);
+  	PrintPoly("Result of addition is: ", Addition.next);
 	PrintPoly("Result of multiplication is: ", Multiplication.next);
-  return EXIT_SUCCESS;
+  	return EXIT_SUCCESS;
 }
 int ReadFile(Position head1, Position head2, char* fileName) {
 	FILE* file = NULL;
 	char buffer[MAX_LINE];
 	file = fopen(fileName, "r");
-  if(!file) {
-    perror("Error!");
-    return -1;
-  }
-  fgets(buffer, MAX_LINE, file);
-  StringToList(head1, buffer);
-  fgets(buffer, MAX_LINE, file);
-  StringToList(head2, buffer);
-  fclose(file);
+  	if(!file) {
+    		perror("Error!");
+    		return -1;
+  	}
+  	fgets(buffer, MAX_LINE, file);
+  	StringToList(head1, buffer);
+  	fgets(buffer, MAX_LINE, file);
+  	StringToList(head2, buffer);
+  	fclose(file);
 	return EXIT_SUCCESS;
 }
 int StringToList(Position head, char* buffer) {
-  char* current = buffer;
-  int coefficient = 0;
-  int exponent = 0;
-  int bytes = 0;
-  Position newElement;
-  while(strlen(current) > 0) {
-    if(sscanf(current, " %d %d %n", &coefficient, &exponent, &bytes) != 2) {
-      perror("File not good!\n");
-      return -1;
-    }
-    newElement = CreateElement(coefficient, exponent);
-    if(!newElement)
-      return EXIT_FAILURE;
-    InsertSorted(head, newElement);
-    current += bytes;
-  }
-  return EXIT_SUCCESS;
+  	char* current = buffer;
+  	int coefficient = 0;
+  	int exponent = 0;
+  	int bytes = 0;
+  	Position newElement;
+  	while(strlen(current) > 0) {
+   		if(sscanf(current, " %d %d %n", &coefficient, &exponent, &bytes) != 2) {
+      			perror("File not good!\n");
+      			return -1;
+    		}
+    	newElement = CreateElement(coefficient, exponent);
+    	if(!newElement)
+      		return EXIT_FAILURE;
+    	InsertSorted(head, newElement);
+    	current += bytes;
+  	}
+  	return EXIT_SUCCESS;
 }
 Position CreateElement(int coefficient, int exponent) {
-  Position element = NULL;
-  element = (Position)malloc(sizeof(Element));
-  if(!element) {
-    perror("Can't allocate memory!\n");
-    return element;
-  }
-  element->coefficient = coefficient;
-  element->exponent = exponent;
-  element->next = NULL;
-  return element;
+  	Position element = NULL;
+  	element = (Position)malloc(sizeof(Element));
+  	if(!element) {
+    		perror("Can't allocate memory!\n");
+    		return element;
+  	}
+  	element->coefficient = coefficient;
+  	element->exponent = exponent;
+  	element->next = NULL;
+  	return element;
 }
 int InsertSorted(Position head, Position newElement) {
-  Position tmp = head;
-  while((tmp->next->exponent && tmp->next != NULL) < newElement->exponent)
-    tmp = tmp->next;
-  MergeAfter(tmp, newElement);
+	Position tmp = head;
+	while((tmp->next->exponent && tmp->next != NULL) < newElement->exponent)
+    	tmp = tmp->next;
+  	MergeAfter(tmp, newElement);
 }
 int MergeAfter(Position position, Position newElement) {
-  if (position->next == NULL || position->next->exponent != newElement->exponent)
-    InsertAfter(position, newElement);
-  else {
-    int addition = position->next->coefficient + newElement->coefficient;
-    if(addition != 0)
-      position->next->coefficient = addition;
-    else
-      DeleteAfter(position);
-    free(newElement);
-  }
-  return EXIT_SUCCESS;
+  	if (position->next == NULL || position->next->exponent != newElement->exponent)
+    	InsertAfter(position, newElement);
+  	else {
+    		int addition = position->next->coefficient + newElement->coefficient;
+    		if(addition != 0)
+      			position->next->coefficient = addition;
+    		else
+      			DeleteAfter(position);
+    		free(newElement);
+  	}
+ 	return EXIT_SUCCESS;
 }
 int InsertAfter(Position position, Position newElement) {
 	newElement->next = position->next;
